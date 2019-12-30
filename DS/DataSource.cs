ac7a8832dc2afcb66208095836e6be1027d6e96d
efidim <edimentm@g.jct.ac.il>
2019-12-30 14:08:05 +0200

    //

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -9,6 +9,7 @@ namespace DS
 {
     public class DataSource
     {
+
         public static List<GuestRequest> GuestRequests { get; set; } = new List<GuestRequest>()
         {
            new GuestRequest(){ GuestRequestKey=4589, PrivateName="yos",FamilyName="lev",MailAddress="yos@org.zehut.il",Status=true,RegistrationDate=new DateTime(2019,12,01),EntryDate=new DateTime(2019,12,02),
@@ -25,14 +26,7 @@ namespace DS
         {
             new Order{HostingUnitKey=469834, GuestRequestKey=4589,OrderKey=123,Status=2,CreateDate=new DateTime(2019, 12, 9), OrderDate=new DateTime(2019, 12, 12),CommissionPerDay=10}
         };
-        bool[,] temp = new bool[12, 31];
-      /*  for(int i = 0; i< 12; i++)
-            {
-                for (int j = 0; j< 31; j++)
-                {
-                    temp[i,j] = false;
-                }
-            }*/
+       
     }
 
 }
