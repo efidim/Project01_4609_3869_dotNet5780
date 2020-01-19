f1ca517dfb64b642db95a949909861053b032fb6
Yossef Levran <yossef.levran@gmail.com>
2020-01-19 21:15:23 +0200

    1

--- PLWPF/AppOwner/AppOwnerWindow.xaml.cs
+++ PLWPF/AppOwner/AppOwnerWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BE;
+using BL;
 
 namespace PLWPF
 {
@@ -21,7 +23,31 @@ namespace PLWPF
     {
         public AppOwnerWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void unitsByAreaButton_Click(object sender, RoutedEventArgs e)
+        {
+            new UnitsByAreaWindow().Show();
+            this.Close();
+        }
+
+        private void unitsByDayButton_Click(object sender, RoutedEventArgs e)
+        {
+            new UnitsByDayWindow().Show();
+            this.Close();
+        }
+
+        private void numOfOrdersButton_Click(object sender, RoutedEventArgs e)
+        {
+            new OrdersByRequestWindow();
+            this.Close();
+        }
+
+        private void HostsByUnitsButton_Click(object sender, RoutedEventArgs e)
+        {
+            new HostByUnitsWindow().Show();
+            this.Close();
         }
     }
 }
